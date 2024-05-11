45d1976c37dff3f0e921946d50a2314d20ad84af
Izabela <izabela.m.alfenas@gmail.com>
2024-05-11 16:45:16 -0300

    finished project, lol

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,61 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
+
+import { registerRestaurante } from './routes/register-restaurante'
+import { sendAuthLink } from './routes/send-auth-link'
+import { authenticateFromLink } from './routes/authenticate-from-link'
+import { signOut } from './routes/sign-out'
+import { getProfile } from './routes/get-profile'
+import { getManagedRestaurante } from './routes/get-managed-restaurante'
+import { getOrderDetails } from './routes/get-order-details'
+import { approveOrder } from './routes/approve-order'
+import { cancelOrder } from './routes/cancel-order'
+import { deliverOrder } from './routes/deliver-order'
+import { dispatchOrder } from './routes/dispatch-order'
+import { getOrders } from './routes/get-orders'
+import { getMonthReceipt } from './routes/get-month.receipt'
+import { getDayOrdersAmount } from './routes/get-day-orders-amount'
+import { getMonthOrdersAmount } from './routes/get-month-orders-amount'
+import { getMonthCanceledOrdersAmount } from './routes/get-month-canceled-orders-amount'
+import { getPopularProducts } from './routes/get-popular-products'
+import { getDailyReceiptInPeriod } from './routes/get-daily-receipt-in-period'
 
 const app = new Elysia()
-  .get('/', () => {
-    return 'Hello World'
-  });
+  .use(registerRestaurante)
+  .use(sendAuthLink)
+  .use(authenticateFromLink)
+  .use(signOut)
+  .use(getProfile)
+  .use(getManagedRestaurante)
+  .use(getOrderDetails)
+  .use(approveOrder)
+  .use(cancelOrder)
+  .use(deliverOrder)
+  .use(dispatchOrder)
+  .use(getOrders)
+  .use(getMonthReceipt)
+  .use(getDayOrdersAmount)
+  .use(getMonthOrdersAmount)
+  .use(getMonthCanceledOrdersAmount)
+  .use(getPopularProducts)
+  .use(getDailyReceiptInPeriod)
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case 'VALIDATION': {
+        set.status = error.status
+
+        return error.toResponse()
+      }
+      case 'NOT_FOUND': {
+        return new Response(null, { status: 404 })
+      }
+      default: {
+        console.error(error)
+
+        return new Response(null, { status: 500 })
+      }
+    }
+  })
 
 app.listen(3333, () => {
-  console.log('Server is running');
-});
+  console.log('🔥 HTTP server running!')
+})
